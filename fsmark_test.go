bf516f8521fa7ce3a29878a137ee45d6db6affe4
Vasiliy Vasilyuk <vasilyukvasiliy@gmail.com>
2018-02-12 23:40:40 +0400

    feat(*): Added licence headers

--- fsmark_test.go
+++ fsmark_test.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2018 Vasiliy Vasilyuk <vasilyukvasiliy@gmail.com>
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package fsmark
 
 import (
